f5c5bf71ad8c6facc14cd866105af5b1abe1f046
DPontes <dpontes@protonmail.com>
2021-04-27 14:05:04 +0200

    not recommended way of applying values to structs

--- main.go
+++ main.go
@@ -10,5 +10,11 @@ type person struct {
 }
 
 func main() {
-  
+  /*
+   The order of values inside {} corresponds to the order of variables in the struct.
+   This is not the best way to assign values to a struct, as the order of the variables
+   declaration inside the struct can be changes at some point
+  */
+  alex := person{"Alex", "Anderson"}
+
 }
